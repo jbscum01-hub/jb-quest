9e063dc1e346490906b06f588694de7bba7c84e7
jbscum01-hub <jb.scum01@gmail.com>
2026-03-10 15:49:56 +0700

    Create bot.js

---
+++ src/bot.js
@@ -0,0 +1,45 @@
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection
+} = require('discord.js');
+
+const { loadEnv } = require('./config/env');
+const { logger } = require('./config/logger');
+const { registerInteractionHandler } = require('./handlers/interactionCreate');
+
+async function createBot() {
+  loadEnv();
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent
+    ],
+    partials: [Partials.Channel, Partials.Message]
+  });
+
+  client.commands = new Collection();
+
+  client.once('ready', async () => {
+    logger.info(`Logged in as ${client.user.tag}`);
+  });
+
+  registerInteractionHandler(client);
+
+  client.on('error', (error) => {
+    logger.error('Discord client error', error);
+  });
+
+  client.on('warn', (message) => {
+    logger.warn(message);
+  });
+
+  return client;
+}
+
+module.exports = {
+  createBot
+};
