1d79f62df19cc0bcefc52aa4a2c1dfd9b8e8f192
jbscum01-hub <jb.scum01@gmail.com>
2026-03-10 15:11:03 +0700

    Update professionPanel.embed.js

--- src/builders/embeds/professionPanel.embed.js
+++ src/builders/embeds/professionPanel.embed.js
@@ -1,1 +1,26 @@
+const { EmbedBuilder } = require('discord.js');
 
+function buildProfessionPanelEmbed(professionCode) {
+  return new EmbedBuilder()
+    .setTitle(`📜 กระดานภารกิจสาย ${professionCode}`)
+    .setDescription(
+      [
+        'ยินดีต้อนรับสู่กระดานภารกิจประจำสายอาชีพ',
+        '',
+        'คุณสามารถดำเนินการได้จากปุ่มด้านล่าง',
+        '• ดูเควสปัจจุบัน',
+        '• ส่งเควสหลัก',
+        '• ส่งเควสซ้ำ',
+        '',
+        'โปรดตรวจสอบรายละเอียดเควสและเตรียมหลักฐานให้พร้อมก่อนส่ง'
+      ].join('\n')
+    )
+    .setFooter({
+      text: 'SCUM Quest System'
+    })
+    .setTimestamp(new Date());
+}
+
+module.exports = {
+  buildProfessionPanelEmbed
+};
