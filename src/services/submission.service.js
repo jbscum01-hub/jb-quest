5071ddddbdd80fc23e98b220b7d82a563033811f
jbscum01-hub <jb.scum01@gmail.com>
2026-03-10 17:10:25 +0700

    Update submission.service.js

--- src/services/submission.service.js
+++ src/services/submission.service.js
@@ -1,68 +1,25 @@
-const { SUBMISSION_STATES } = require('../constants/questStates');
-const { findOrCreatePlayerProfile } = require('../db/queries/playerProfile.repo');
 const {
-  findCurrentMainQuestByProfession,
-  findRepeatableQuestsByProfession
-} = require('../db/queries/questMaster.repo');
-const { createSubmission } = require('../db/queries/submission.repo');
-
-async function resolveQuestForSubmission({ professionCode, submissionMode }) {
-  if (submissionMode === 'MAIN') {
-    return findCurrentMainQuestByProfession(professionCode);
-  }
-
-  if (submissionMode === 'REPEATABLE') {
-    const quests = await findRepeatableQuestsByProfession(professionCode);
-    return quests[0] || null;
-  }
-
-  return null;
-}
+  createSubmission
+} = require('../db/queries/submission.repo');
 
 async function submitQuest({
-  discordUserId,
-  discordUsername,
-  professionCode,
-  submissionMode,
-  title,
-  description,
-  proofText
+  playerId,
+  professionId,
+  questId,
+  submissionType,
+  ingameName,
+  text
 }) {
-  const playerProfile = await findOrCreatePlayerProfile({
-    discordUserId,
-    discordUsername
+  return createSubmission({
+    playerId,
+    professionId,
+    questId,
+    submissionType,
+    ingameName,
+    submissionText: text
   });
-
-  const quest = await resolveQuestForSubmission({
-    professionCode,
-    submissionMode
-  });
-
-  if (!quest) {
-    throw new Error('ไม่พบเควสที่สามารถส่งได้สำหรับสายนี้');
-  }
-
-  const submission = await createSubmission({
-    playerProfileId: playerProfile.id,
-    questId: quest.id,
-    submissionType: submissionMode,
-    submissionState: SUBMISSION_STATES.PENDING_REVIEW,
-    professionCode,
-    title,
-    description,
-    proofText,
-    submittedByDiscordId: discordUserId,
-    submittedByDiscordTag: discordUsername
-  });
-
-  return {
-    playerProfile,
-    quest,
-    submission
-  };
 }
 
 module.exports = {
-  resolveQuestForSubmission,
   submitQuest
 };
