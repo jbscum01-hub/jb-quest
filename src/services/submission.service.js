b64875e64207738745fb49760b579115aed6e159
jbscum01-hub <jb.scum01@gmail.com>
2026-03-10 17:40:28 +0700

    Update submission.service.js

--- src/services/submission.service.js
+++ src/services/submission.service.js
@@ -1,25 +1,40 @@
-const {
-  createSubmission
-} = require('../db/queries/submission.repo');
+const { getPool } = require('../db/pool');
 
-async function submitQuest({
+async function createQuestSubmission({
   playerId,
   professionId,
   questId,
   submissionType,
   ingameName,
-  text
+  submissionText
 }) {
-  return createSubmission({
+
+  const pool = getPool();
+
+  const result = await pool.query(`
+    INSERT INTO tb_quest_submission
+    (
+      player_id,
+      profession_id,
+      quest_id,
+      submission_type,
+      player_ingame_name,
+      submission_text
+    )
+    VALUES ($1,$2,$3,$4,$5,$6)
+    RETURNING *
+  `,[
     playerId,
     professionId,
     questId,
     submissionType,
     ingameName,
-    submissionText: text
-  });
+    submissionText
+  ]);
+
+  return result.rows[0];
 }
 
 module.exports = {
-  submitQuest
+  createQuestSubmission
 };
