194ef835c00c55b59dc468b60e842b89be1f6dff
jbscum01-hub <jb.scum01@gmail.com>
2026-03-10 15:06:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+const { createBot } = require('./bot');
+const { logger } = require('./config/logger');
 
+async function bootstrap() {
+  try {
+    const client = await createBot();
+    await client.login(process.env.BOT_TOKEN);
+    logger.info('SCUM Quest Bot started successfully');
+  } catch (error) {
+    logger.error('Failed to start bot', error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
