cf6a73f9365c44595cd623f48c4e6eaedd86dbdb
jbscum01-hub <jb.scum01@gmail.com>
2026-03-11 15:10:37 +0700

    Update questSubmission.modal.js

--- src/handlers/modals/questSubmission.modal.js
+++ src/handlers/modals/questSubmission.modal.js
@@ -1,57 +1,25 @@
-const { buildReviewCardEmbed } = require('../../builders/embeds/reviewCard.embed');
-const { buildReviewCardComponents } = require('../../builders/components/reviewCard.components');
-const { DISCORD_CONFIG_KEYS } = require('../../constants/discordConfigKeys');
-const { getGlobalConfigValue } = require('../../services/discordConfig.service');
-const { submitQuest } = require('../../services/submission.service');
+const { logger } = require('../../config/logger');
 
-async function handleQuestSubmissionModal(interaction, parsedCustomId) {
-  const submissionMode = parsedCustomId.action;
-  const professionCode = parsedCustomId.extra;
+async function handleQuestSubmissionModal(interaction, parsed) {
 
-  const ingameName = interaction.fields.getTextInputValue('submission_ingame_name');
-  const submissionText = interaction.fields.getTextInputValue('submission_text');
+  await interaction.deferReply({ ephemeral: true });
 
-  const attachments = [];
+  const characterName =
+    interaction.fields.getTextInputValue('character_name');
 
-  const result = await submitQuest({
-    discordUserId: interaction.user.id,
-    discordUsername: interaction.user.tag,
-    discordDisplayName: interaction.member?.displayName || interaction.user.username,
-    professionCode,
-    submissionMode,
-    ingameName,
-    submissionText,
-    attachments
+  const screenshot =
+    interaction.fields.getTextInputValue('screenshot');
+
+  console.log("Quest Submit", {
+    user: interaction.user.id,
+    characterName,
+    screenshot
   });
 
-  const reviewChannelId = await getGlobalConfigValue(DISCORD_CONFIG_KEYS.QUEST_REVIEW_CHANNEL);
-
-  if (reviewChannelId) {
-    const reviewChannel = await interaction.client.channels.fetch(reviewChannelId).catch(() => null);
-
-    if (reviewChannel) {
-      const reviewEmbed = buildReviewCardEmbed({
-        submission: result.submission,
-        quest: result.quest,
-        playerProfile: result.playerProfile,
-        memberDisplayName: interaction.member?.displayName || interaction.user.username
-      });
-
-      const reviewComponents = buildReviewCardComponents(result.submission.submission_id);
-
-      await reviewChannel.send({
-        embeds: [reviewEmbed],
-        components: reviewComponents
-      });
-    }
-  }
-
-  await interaction.reply({
-    content: submissionMode === 'MAIN'
-      ? `ส่งเควสหลักของสาย ${professionCode} เรียบร้อยแล้ว รอแอดมินตรวจสอบ`
-      : `ส่งเควสซ้ำของสาย ${professionCode} เรียบร้อยแล้ว รอแอดมินตรวจสอบ`,
-    ephemeral: true
+  await interaction.editReply({
+    content: 'ส่งเควสเรียบร้อยแล้ว ทีมงานจะตรวจสอบให้'
   });
+
 }
 
 module.exports = {
