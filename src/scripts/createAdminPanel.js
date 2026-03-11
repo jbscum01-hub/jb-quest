8cf129e2fb79e9313eadc377ce2ae28c8824e2c8
jbscum01-hub <jb.scum01@gmail.com>
2026-03-11 14:30:12 +0700

    Update createAdminPanel.js

--- src/scripts/createAdminPanel.js
+++ src/scripts/createAdminPanel.js
@@ -1,75 +1,15 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { createBot } = require('../bot');
+const { autoDeployAdminPanel } = require('../services/adminPanelAutoDeploy.service');
 
-const { buildAdminPanelEmbed } = require('../builders/embeds/adminPanel.embed');
-const { buildAdminPanelButtons } = require('../builders/components/adminPanel.components');
+async function run() {
+  const client = await createBot();
 
-const { getPool } = require('../db/pool');
-
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const GUILD_ID = process.env.GUILD_ID;
-
-async function getAdminPanelChannelId() {
-
-  const pool = getPool();
-
-  const result = await pool.query(`
-    SELECT config_value
-    FROM tb_quest_master_discord_config
-    WHERE config_key = 'QUEST_ADMIN_PANEL_CHANNEL'
-    LIMIT 1
-  `);
-
-  if (!result.rows.length) {
-    throw new Error('QUEST_ADMIN_PANEL_CHANNEL not found in config');
-  }
-
-  return result.rows[0].config_value;
-
-}
-
-async function createAdminPanel() {
-
-  const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
-  });
-
-  client.once('ready', async () => {
-
-    try {
-
-      console.log(`Bot logged in as ${client.user.tag}`);
-
-      const channelId = await getAdminPanelChannelId();
-
-      const guild = await client.guilds.fetch(GUILD_ID);
-      const channel = await guild.channels.fetch(channelId);
-
-      if (!channel) {
-        throw new Error('Admin panel channel not found');
-      }
-
-      const embed = buildAdminPanelEmbed();
-      const components = buildAdminPanelButtons();
-
-      const message = await channel.send({
-        embeds: [embed],
-        components
-      });
-
-      console.log('Admin panel created:', message.id);
-
-    } catch (err) {
-
-      console.error('Create admin panel failed', err);
-
-    }
-
+  client.once('clientReady', async () => {
+    await autoDeployAdminPanel(client);
     process.exit(0);
-
   });
 
-  await client.login(BOT_TOKEN);
-
+  await client.login(process.env.BOT_TOKEN);
 }
 
-createAdminPanel();
+run();
