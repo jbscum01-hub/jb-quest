33c536f389960b35aa9e79afd63d75d325c743aa
jbscum01-hub <jb.scum01@gmail.com>
2026-03-11 14:27:26 +0700

    Update adminPanel.service.js

--- src/services/adminPanel.service.js
+++ src/services/adminPanel.service.js
@@ -1,27 +1,13 @@
 const { buildAdminPanelEmbed } = require('../builders/embeds/adminPanel.embed');
 const { buildAdminPanelButtons } = require('../builders/components/adminPanel.components');
 
-async function sendAdminPanel(channel) {
-  const embed = buildAdminPanelEmbed();
-  const components = buildAdminPanelButtons();
-
-  await channel.send({
-    embeds: [embed],
-    components
-  });
-}
-
 async function refreshAdminPanel(message) {
-  const embed = buildAdminPanelEmbed();
-  const components = buildAdminPanelButtons();
-
   await message.edit({
-    embeds: [embed],
-    components
+    embeds: [buildAdminPanelEmbed()],
+    components: buildAdminPanelButtons()
   });
 }
 
 module.exports = {
-  sendAdminPanel,
   refreshAdminPanel
 };
