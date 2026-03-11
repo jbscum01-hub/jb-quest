14052f582683a7583d6eb642e9b2097a60aec48e
jbscum01-hub <jb.scum01@gmail.com>
2026-03-11 14:20:03 +0700

    Update permission.js

--- src/utils/permission.js
+++ src/utils/permission.js
@@ -2,15 +2,10 @@ const { DISCORD_CONFIG_KEYS } = require('../constants/discordConfigKeys');
 const { getGlobalConfigValue } = require('../services/discordConfig.service');
 
 async function isQuestAdmin(member) {
-  if (!member) {
-    return false;
-  }
+  if (!member) return false;
 
   const adminRoleId = await getGlobalConfigValue(DISCORD_CONFIG_KEYS.QUEST_ADMIN_ROLE);
-
-  if (!adminRoleId) {
-    return false;
-  }
+  if (!adminRoleId) return false;
 
   return member.roles.cache.has(adminRoleId);
 }
